6028cd929b66548fad0a076a8cf7b5bbffb58f0f
Ma-Vin <ma.vin.github@gmail.com>
2025-03-14 22:33:43 +0100

    feat: #23 create cron file renaming

---
+++ appender/file_renamer.go
@@ -0,0 +1,84 @@
+package appender
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/ma-vin/typewriter/common"
+)
+
+type CronFileNamer struct {
+	pathToLogFile         string
+	writer                *os.File
+	crontab               *common.Crontab
+	timeFileNameGenerator *TimeFileNameGenerator
+	mu                    *sync.Mutex
+}
+
+type TimeFileNameGenerator struct {
+	basePath      string
+	fileEnding    string
+	referenceTime *time.Time
+}
+
+// Creates a new CronFileNamer for a given path and crontab
+func CreateCronFileRenamer(pathToLogFile string, writer *os.File, crontab *common.Crontab, mu *sync.Mutex) *CronFileNamer {
+	indexOfFileEnding := strings.LastIndex(pathToLogFile, ".")
+	refTime := time.Now()
+	fileNameCreator := TimeFileNameGenerator{pathToLogFile[:indexOfFileEnding], pathToLogFile[indexOfFileEnding+1:], &refTime}
+	return &CronFileNamer{pathToLogFile, writer, crontab, &fileNameCreator, mu}
+}
+
+// Checks whether the next time of crontab is reached or not. In positive case the current file will be renamed to a name given by filename generator.
+func (c *CronFileNamer) CheckFile(logValues *common.LogValues) {
+	if logValues.Time.Before(*c.crontab.NextTime) {
+		return
+	}
+
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if _, err := os.Stat(c.pathToLogFile); err != nil {
+		return
+	}
+
+	newPath := c.timeFileNameGenerator.determineNextPathToLogFile()
+	err := os.Rename(c.pathToLogFile, newPath)
+	if err != nil {
+		fmt.Println("Failed to rename log file from", c.pathToLogFile, "to", newPath)
+	}
+
+	if !SkipFileCreationForTest {
+		file, err := os.OpenFile(c.pathToLogFile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+		if err == nil {
+			*c.writer = *file
+		}
+	}
+	if err != nil {
+		fmt.Printf("Fail to create file appender, use stdout instead: %s", err)
+		fmt.Println()
+	}
+
+	c.timeFileNameGenerator.referenceTime = c.crontab.NextTime
+	c.crontab.CalculateNextTime()
+}
+
+// creates the next path of log file
+func (t *TimeFileNameGenerator) determineNextPathToLogFile() string {
+	return fmt.Sprintf("%s_%d%s%s_%s%s%s.%s", t.basePath,
+		t.referenceTime.Year(), determineTwoDigits(int(t.referenceTime.Month())), determineTwoDigits(t.referenceTime.Day()),
+		determineTwoDigits(t.referenceTime.Hour()), determineTwoDigits(t.referenceTime.Minute()), determineTwoDigits(t.referenceTime.Second()),
+		t.fileEnding)
+}
+
+// puts a zero at front if the number is lower than ten
+func determineTwoDigits(number int) string {
+	if number < 10 {
+		return "0" + strconv.Itoa(number)
+	}
+	return strconv.Itoa(number)
+}
---
+++ appender/file_renamer_test.go
@@ -0,0 +1,115 @@
+package appender
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/ma-vin/typewriter/common"
+	"github.com/ma-vin/typewriter/testutil"
+)
+
+var fileRenamerCrontab = common.CreateCrontab("* * * * *")
+var fileRenamerTestTime = time.Date(2025, time.March, 14, 20, 1, 0, 0, time.UTC)
+var fileRenamerMu = sync.Mutex{}
+var logValues = &common.LogValues{Time: fileRenamerTestTime}
+
+func getCronFileSchedulerTestLogFile(testCase string) string {
+	SkipFileCreationForTest = false
+	_, filename, _, _ := runtime.Caller(0)
+	result := strings.Replace(filename, ".go", "_"+testCase+"_scratch.log", 1)
+	os.Create(result)
+	return result
+}
+
+func TestCheckFileNoRename(t *testing.T) {
+	logFileName := getCronFileSchedulerTestLogFile("noRename")
+	file, err := os.OpenFile(logFileName, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+	testutil.AssertNil(err, t, "create log file")
+
+	fileRenamerCrontab.NextTime = timePtr(fileRenamerTestTime.Add(time.Second))
+	scheduler := CreateCronFileRenamer(logFileName, file, fileRenamerCrontab, &fileRenamerMu)
+	scheduler.timeFileNameGenerator.referenceTime = &fileRenamerTestTime
+
+	logFileName = filepath.Base(logFileName)
+	os.Remove(logFileName)
+
+	entriesBefore, err := os.ReadDir("./")
+	testutil.AssertNil(err, t, "read dir before")
+
+	scheduler.CheckFile(logValues)
+
+	entriesAfter, err := os.ReadDir("./")
+	testutil.AssertNil(err, t, "read dir after")
+
+	testutil.AssertEquals(len(entriesBefore), len(entriesAfter), t, "compare dirs")
+	for _, dirEntryBefore := range entriesBefore {
+		contains := false
+		for _, dirEntryAfter := range entriesAfter {
+			contains = contains || dirEntryBefore.Name() == dirEntryAfter.Name()
+		}
+		testutil.AssertTrue(contains, t, "missing "+dirEntryBefore.Name()+" at entriesAfter")
+	}
+}
+
+func TestCheckFileRename(t *testing.T) {
+	logFileName := getCronFileSchedulerTestLogFile("rename")
+	os.Remove(logFileName)
+	file, err := os.OpenFile(logFileName, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+	testutil.AssertNil(err, t, "create log file")
+
+	fileRenamerCrontab.NextTime = timePtr(fileRenamerTestTime.Add(-time.Second))
+	renamer := CreateCronFileRenamer(logFileName, file, fileRenamerCrontab, &fileRenamerMu)
+	renamer.timeFileNameGenerator.referenceTime = &fileRenamerTestTime
+
+	fmt.Fprintln(file, "first test entry")
+
+	logFileName = filepath.Base(logFileName)
+	indexOfFileEnding := strings.LastIndex(logFileName, ".")
+	expectedNewFileName := logFileName[:indexOfFileEnding] + "_20250314_200100" + logFileName[indexOfFileEnding:]
+	os.Remove(expectedNewFileName)
+
+	entriesBefore, err := os.ReadDir("./")
+	testutil.AssertNil(err, t, "read dir before")
+
+	renamer.CheckFile(logValues)
+
+	entriesAfter, err := os.ReadDir("./")
+	testutil.AssertNil(err, t, "read dir after")
+
+	testutil.AssertEquals(len(entriesBefore)+1, len(entriesAfter), t, "compare dirs")
+
+	for _, dirEntryBefore := range entriesBefore {
+		contains := false
+		for _, dirEntryAfter := range entriesAfter {
+			contains = contains || dirEntryBefore.Name() == dirEntryAfter.Name() || logFileName == dirEntryBefore.Name()
+		}
+		testutil.AssertTrue(contains, t, "missing "+dirEntryBefore.Name()+" at entriesAfter")
+	}
+
+	contains := false
+	for _, dirEntryAfter := range entriesAfter {
+		contains = contains || expectedNewFileName == dirEntryAfter.Name()
+	}
+	testutil.AssertTrue(contains, t, "missing "+expectedNewFileName+" at entriesAfter")
+
+	_, err = fmt.Fprintln(file, "second test entry")
+	testutil.AssertNil(err, t, "write second entry")
+
+	dat, err := os.ReadFile(expectedNewFileName)
+	testutil.AssertNil(err, t, "read renamed log file")
+	testutil.AssertEquals("first test entry", strings.TrimSpace(string(dat)), t, "wrong first entry")
+
+	dat, err = os.ReadFile(logFileName)
+	testutil.AssertNil(err, t, "read original named log file")
+	testutil.AssertEquals("second test entry", strings.TrimSpace(string(dat)), t, "wrong second entry")
+}
+
+func timePtr(t time.Time) *time.Time {
+	return &t
+}
